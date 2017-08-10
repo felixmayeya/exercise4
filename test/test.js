6e3d59921921241b1372de4149d8a8eb62e7b2c5
felixmayeya <felixmayeya@163.com>
2017-08-10 15:46:35 +0800

    this

--- test/test.js
+++ test/test.js
@@ -4,7 +4,15 @@ describe('this', function () {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          /*
+            箭头函数：
+            this指向的固定化，并不是因为箭头函数内部有绑定this的机制，
+            实际原因是箭头函数根本没有自己的this，
+            导致内部的this就是外层代码块的this。
+            如果不是箭头函数：
+            this将指向global
+           */
+          this.should.equal(obj)
           done()
         }, 0)
       }
@@ -15,7 +23,8 @@ describe('this', function () {
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      // test运行环境在global，所以this指向到global，如果在浏览器运行，this指向window
+      this.should.deepEqual(global)
     }
     test()
   })
@@ -26,7 +35,10 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            /*
+            在绑定obj时，obj是undefined，所以this就指向了global
+             */
+            this.should.equal(global)
           }
           return _say.bind(obj)
         }()
@@ -39,7 +51,10 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          /*
+          bind 当绑定对象被调用时，该参数会作为原函数运行时的 this 指向
+           */
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
